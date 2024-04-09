b33e40fc36f5bbdf0ad0c64d1a4bc34235641741
EC2 Default User <ec2-user@ip-172-31-19-134.ap-northeast-1.compute.internal>
2024-04-09 12:39:49 +0000

    アプリを作成開始

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
